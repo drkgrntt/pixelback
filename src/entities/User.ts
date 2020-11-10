d49bd89dc00e699709a722500fdeb7f05d8aac53
drkgrntt <drkgrntt@gmail.com>
2020-11-09 20:52:48 -0600

    set up apollo server

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,21 +1,27 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm'
+import { ObjectType, Field } from 'type-graphql'
 import { UserRole } from '../enums/UserRole'
 
+@ObjectType()
 @Entity()
-export class User {
+export class User extends BaseEntity {
 
-  @PrimaryGeneratedColumn()
+  @Field()
+  @PrimaryGeneratedColumn("uuid")
   id!: string
 
+  @Field()
   @Column({ unique: true })
   email: string
 
   @Column()
   password: string
 
+  @Field()
   @Column()
   role: UserRole
 
+  @Field()
   @Column()
   displayName: string
 }
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import "reflect-metadata"
 import "dotenv-safe/config"
 import path from "path"
 import { createConnection } from "typeorm"
+import { buildSchema } from "type-graphql"
+import express from "express"
+import cors from "cors"
+import { ApolloServer } from "apollo-server-express"
+import jwt from "jsonwebtoken"
+import { User } from "./entities/User"
 
 const main = async () => {
-  const connection = await createConnection({
+  await createConnection({
     type: "postgres",
     url: process.env.DATABASE_URL,
     synchronize: true,
@@ -16,7 +22,41 @@ const main = async () => {
     subscribers: []
   })
 
-  console.log(connection)
+  const app = express()
+  app.use(cors())
+
+  const server = new ApolloServer({
+    introspection: true,
+    playground: true,
+    schema: await buildSchema({
+      resolvers: [] as any,
+      validate: false
+    }),
+    context: async ({ req, res }) => {
+
+      let me
+
+      const { token } = req.headers
+      if (typeof token === 'string') {
+        const tokenData = jwt.verify(token, process.env.JWT_SECRET as string)
+        if (typeof tokenData === 'object') {
+          me = await User.findOne((tokenData as { uid: string }).uid)
+        }
+      }
+  
+      return {
+        req,
+        res,
+        me
+      }
+    }
+  })
+
+  server.applyMiddleware({ app })
+
+  app.listen(parseInt(process.env.PORT as string), () => {
+    console.log(`Server started on ${process.env.PORT}`)
+  })
 }
 
 try { 
