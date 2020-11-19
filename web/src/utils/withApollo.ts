54f002f5a3ca049a1442f50999c03e8e98d3bf8b
drkgrntt <drkgrntt@gmail.com>
2020-11-19 16:46:19 -0600

    Add the auth link

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -1,10 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { withApollo } from 'next-apollo'
 
+const httpLink = createHttpLink({
+  uri: process.env.NEXT_PUBLIC_API_URL
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+})
 
 const client = new ApolloClient({
-  uri: "http://localhost:5757/graphql",
-  cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 })
 
 export default withApollo(client)
