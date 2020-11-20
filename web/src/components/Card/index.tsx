9b868098ed15d8242d72ce508c1838329296cf2f
drkgrntt <drkgrntt@gmail.com>
2020-11-20 07:59:33 -0600

    Handle non-logged-in profile

--- web/src/components/Card/index.tsx
+++ web/src/components/Card/index.tsx
@@ -1,12 +1,12 @@
 import styles from './Card.module.scss'
 
 interface Props {
-
+  className?: string
 }
 
 const Card: React.FC<Props> = (props) => {
   return (
-    <div className={styles.card}>
+    <div className={`${styles.card} ${props.className}`}>
       {props.children}
     </div>
   )
--- web/src/pages/profile/index.tsx
+++ web/src/pages/profile/index.tsx
@@ -2,14 +2,25 @@ import { useContext } from 'react'
 import userContext from '../../context/userContext'
 import styles from './Profile.module.scss'
 import Button from '../../components/Button'
+import Card from '../../components/Card'
 import { useLogoutEverywhereMutation } from '../../hooks/useLogoutEverywhereMutation'
+import Link from 'next/link'
 
 const Profile: React.FC<{}> = () => {
 
   const { currentUser, setCurrentUser } = useContext(userContext)
   const [logoutEverywhere] = useLogoutEverywhereMutation()
 
-  if (!currentUser) return null
+  if (!currentUser) {
+    return (
+      <Card className={styles.login}>
+        <Link href="/login">
+          <a className={styles.login}>Login</a>
+        </Link>
+        {' '}to view your profile.
+      </Card>
+    )
+  }
 
   const onLogoutEverywhereClick = async (event: any, reset: Function) => {
     try {
@@ -24,10 +35,13 @@ const Profile: React.FC<{}> = () => {
   return (
     <div className={styles.profile}>
       <h2>Profile</h2>
-      <p>Discard all other authentication tokens to log out everywhere.</p>
-      <Button styleTypes={['delete']} onClick={onLogoutEverywhereClick}>
-        Discard
-      </Button>
+
+      <Card className={styles.card}>
+        <label>Discard all other authentication tokens to log out everywhere.</label>
+        <Button styleTypes={['delete']} onClick={onLogoutEverywhereClick}>
+          Discard
+        </Button>
+      </Card>
     </div>
   )
 }
