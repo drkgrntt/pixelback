50b9cf79237f972891829bace2f6b685ed1fe789
drkgrntt <drkgrntt@gmail.com>
2020-11-19 21:38:09 -0600

    header and footer styles

--- web/src/components/Layout/Footer/index.tsx
+++ web/src/components/Layout/Footer/index.tsx
@@ -3,7 +3,8 @@ import styles from './Footer.module.scss'
 const Footer: React.FC<{}> = () => {
   return (
     <footer className={styles.footer}>
-
+      <h2 className={styles.title}>Pixelback</h2>
+      <span className={styles.copyright}>© drkgrntt 2020</span>
     </footer>
   )
 }
--- web/src/components/Layout/Header/index.tsx
+++ web/src/components/Layout/Header/index.tsx
@@ -3,7 +3,10 @@ import styles from './Header.module.scss';
 const Header: React.FC<{}> = () => {
   return (
     <header className={styles.header}>
-
+      <h1 className={styles.title}>
+        Pixelback
+        <span className={styles.subtitle}>A platform for writers</span>
+      </h1>
     </header>
   )
 }
