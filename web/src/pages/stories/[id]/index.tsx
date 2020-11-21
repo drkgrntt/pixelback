b0cc7a065dbfd08ed937c07ecb4c3857c43945ac
drkgrntt <drkgrntt@gmail.com>
2020-11-21 16:57:18 -0600

    Load the story to the story page

--- web/src/pages/stories/[id]/index.tsx
+++ web/src/pages/stories/[id]/index.tsx
@@ -1,12 +1,37 @@
-import { Story } from '../../../types'
+import Error from 'next/error'
+import { NextPage } from 'next'
 import styles from './[id].module.scss'
+import Loader from '../../../components/Loader'
+import { useStoryQuery } from '../../../hooks/useStoryQuery'
+import withApollo from '../../../utils/withApollo'
+import { ParsedUrlQuery } from 'querystring'
 
-const StoryPage: React.FC<{}> = () => {
+interface Props {
+  query: ParsedUrlQuery
+}
+
+const StoryPage: NextPage<Props> = (props) => {
+
+  const result = useStoryQuery({ id: props.query.id as string })
+  
+  switch (true) {
+    case !!result.error:
+      return <Error statusCode={404} />
+    case !!result.loading:
+      return <Loader />
+  }
+      
+  const story = result.data.story
+
   return (
     <div>
-      Story page
+      <h2>{story.title}</h2>
     </div>
   )
+}
+
+StoryPage.getInitialProps = ({ query }) => {
+  return { query }
 }
 
 export default StoryPage
