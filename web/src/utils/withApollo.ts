fae4284e12578233b8fcb2c835863991f6c46d70
drkgrntt <drkgrntt@gmail.com>
2020-11-21 09:19:12 -0600

    Convert date strings from the server to date objects

--- web/src/utils/withApollo.ts
+++ web/src/utils/withApollo.ts
@@ -2,6 +2,7 @@ import {
   ApolloClient,
   InMemoryCache,
   createHttpLink,
+  ApolloLink
 } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { withApollo } from 'next-apollo'
@@ -20,9 +21,49 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+/**
+ * The server will return dates as stringified unix timestanps,
+ * so this will to convert those to date objects
+ */
+const convertDates = (data: any) => {
+  Object.keys(data).forEach(key => {
+    switch (typeof data[key]) {
+      case 'string':
+        if (
+          key === 'createdAt' ||
+          key === 'publishedAt' ||
+          key === 'updatedAt'
+        ) {
+          // This is what we came here to do
+          data[key] = new Date(parseInt(data[key]))
+        }
+        break
+      case 'object':
+        if (Array.isArray(data[key])) {
+          data[key].forEach((item: any) => convertDates(item))
+          break
+        }
+        convertDates(data[key])
+        break
+    }
+  })
+
+  return data
+}
+
+const dataMutationLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map((data) => {
+    data = convertDates(data)
+    return data
+  })
+})
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: dataMutationLink.concat(authLink).concat(httpLink),
   cache: new InMemoryCache(),
+  name: 'react-web-client',
+  version: '1.0.0',
+  ssrMode: true
 })
 
 export default withApollo(client)
