60a94bef92f6880ef4e5a0abe1ff69f3d45d0c21
drkgrntt <drkgrntt@gmail.com>
2020-11-21 15:49:35 -0600

    create dummy story component

---
+++ web/src/pages/stories/[id]/index.tsx
@@ -0,0 +1,12 @@
+import { Story } from '../../../types'
+import styles from './[id].module.scss'
+
+const Story: React.FC<{}> = () => {
+  return (
+    <div>
+      Story page
+    </div>
+  )
+}
+
+export default Story
