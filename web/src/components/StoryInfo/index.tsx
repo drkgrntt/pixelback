1f3edaf0fa0bd516f917e29f7a088be1f7bf72d7
drkgrntt <drkgrntt@gmail.com>
2020-11-22 10:55:43 -0600

    Add date info to story info

--- web/src/components/StoryInfo/index.tsx
+++ web/src/components/StoryInfo/index.tsx
@@ -19,6 +19,9 @@ const StoryInfo: React.FC<Props> = ({ story }) => {
         </li>
         <li>Score: {story.score}/5</li>
         <li>Status: {PublishStatus[story.status]}</li>
+        <li>Created: {story.createdAt.toLocaleDateString()}</li>
+        <li>Last updated: {story.updatedAt.toLocaleDateString()}</li>
+        {story.publishedAt && <li>Published: {story.publishedAt.toLocaleDateString()}</li>}
       </ul>
     </>
   )
