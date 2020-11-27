6c346f4abd1c6ba1abc7b8be2c6c1650e99c2cb0
drkgrntt <drkgrntt@gmail.com>
2020-11-27 10:31:04 -0600

    Separate story list into its own component

---
+++ web/src/components/StoryList/index.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import Card from '@/components/Card'
+import { Story } from '@/types'
+import styles from './StoryList.module.scss'
+
+interface Props {
+  stories: Story[]
+}
+
+const StoryList: React.FC<Props> = ({ stories }) => {
+  const renderRating = (story: Story) => {
+    if (story.ratings.length < 5) return null
+
+    return <p>Rated {story.score}/5</p>
+  }
+
+  const renderStories = () => {
+    return stories.map((story: Story) => {
+      return (
+        <Card key={story.id}>
+          <Link href={`/stories/${story.id}`}>
+            <h3>{story.title}</h3>
+          </Link>
+          {renderRating(story)}
+          {story.summary}
+          <p>By {story.author.displayName}</p>
+          <Link href={`/stories/${story.id}`}>
+            <a>Read</a>
+          </Link>
+        </Card>
+      )
+    })
+  }
+
+  return <>{renderStories()}</>
+}
+
+export default StoryList
--- web/src/pages/stories/index.tsx
+++ web/src/pages/stories/index.tsx
@@ -1,12 +1,10 @@
 import { NextPage } from 'next'
-import Link from 'next/link'
 import { withApollo } from '@/utils/withApollo'
 import { useStoriesQuery } from '@/queries/useStoriesQuery'
 import styles from './Stories.module.scss'
 import Loader from '@/components/Loader'
-import Card from '@/components/Card'
-import { Story } from '@/types'
 import Button from '@/components/Button'
+import StoryList from '@/components/StoryList'
 
 interface Props {}
 
@@ -22,30 +20,6 @@ const Stories: NextPage<Props> = (props) => {
   }
 
   const stories = data?.stories.stories
-
-  const renderRating = (story: Story) => {
-    if (story.ratings.length < 5) return null
-
-    return <p>Rated {story.score}/5</p>
-  }
-
-  const renderStories = () => {
-    return stories.map((story: Story) => {
-      return (
-        <Card key={story.id}>
-          <Link href={`/stories/${story.id}`}>
-            <h3>{story.title}</h3>
-          </Link>
-          {renderRating(story)}
-          {story.summary}
-          <p>By {story.author.displayName}</p>
-          <Link href={`/stories/${story.id}`}>
-            <a>Read</a>
-          </Link>
-        </Card>
-      )
-    })
-  }
 
   const loadMore = (event: any, reset: Function) => {
     event.preventDefault()
@@ -66,7 +40,7 @@ const Stories: NextPage<Props> = (props) => {
   return (
     <div>
       <h2>Stories</h2>
-      {renderStories()}
+      <StoryList stories={stories} />
       {renderMore()}
     </div>
   )
