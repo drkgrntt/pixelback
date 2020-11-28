6ae73a67c707d8fba84bf68cf37c2d30165fb8b1
drkgrntt <drkgrntt@gmail.com>
2020-11-28 11:28:59 -0600

    improve star scale to be based on quarter stars

--- web/src/components/StarScale/index.tsx
+++ web/src/components/StarScale/index.tsx
@@ -4,19 +4,49 @@ import styles from './StarScale.module.scss'
 interface Props {
   score: number
 }
-
+/**
+ * For each star:
+ * 0-0.124 = empty |
+ * 0.125-0.374 = quarter |
+ * 0.375-0.624 = half |
+ * 0.625-0.874 = three quarters |
+ * 0.875-1 = full
+ */
 const StarScale: React.FC<Props> = ({ score }) => {
+  const rem = score % 1
   const renderStar = (pos: number) => {
-    if (pos-.25 < score) {
+    // Full star
+    if (pos <= score || (pos - 1 < score && rem >= 0.875)) {
       return (
         <i
           className={`${styles.star} fa fa-star`}
           aria-hidden="true"
         />
       )
-    } else if (pos > score && pos-1 < score && pos-score > .25 && pos-score < .75) {
+      // Empty star
+    } else if (pos - 1 > score || (pos > score && rem < 0.125)) {
       return (
-        <div className={styles.half}>
+        <i
+          className={`${styles.star} ${styles.new} fa fa-star`}
+          aria-hidden="true"
+        />
+      )
+      // Star fractions
+    } else {
+      let phase = ''
+      switch (true) {
+        case rem >= 0.125 && rem < 0.375:
+          phase = 'crescent'
+          break
+        case rem >= 0.625 && rem < 0.875:
+          phase = 'gibbous'
+          break
+        default:
+          phase = 'half'
+          break
+      }
+      return (
+        <div className={styles[phase]}>
           <i
             className={`${styles.star} fa fa-star`}
             aria-hidden="true"
@@ -26,13 +56,6 @@ const StarScale: React.FC<Props> = ({ score }) => {
             aria-hidden="true"
           />
         </div>
-      )
-    } else {
-      return (
-        <i
-          className={`${styles.star} ${styles.empty} fa fa-star`}
-          aria-hidden="true"
-        />
       )
     }
   }
