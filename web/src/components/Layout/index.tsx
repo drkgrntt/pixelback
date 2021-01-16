2472bbf1f3c1c5319f624338985b2b8ae80e1961
drkgrntt <drkgrntt@gmail.com>
2021-01-16 16:47:36 -0600

    Add notification component and cookie notification

--- web/src/components/Layout/index.tsx
+++ web/src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import styles from './Layout.module.scss'
+import Notification from './Notification'
 import Nav from './Nav'
 import Header from './Header'
 import Footer from './Footer'
@@ -10,6 +11,13 @@ const Layout: React.FC<{}> = (props) => {
       <Header />
       <main className={styles.layout}>{props.children}</main>
       <Footer />
+      <Notification
+        id="cookies"
+        title="This site uses cookies."
+        content="To learn more, read our <a href='/private-policy'>Private Policy</a>"
+        type="session"
+        closeText="I accept"
+      />
     </>
   )
 }
