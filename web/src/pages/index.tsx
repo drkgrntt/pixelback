4ad0bf5a7bc42d2ea90db178044a03c80e2a2951
drkgrntt <drkgrntt@gmail.com>
2021-01-24 08:35:18 -0600

    redirect from landing

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,19 @@
+import { useEffect } from 'react'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import styles from './Home.module.scss'
 import Button from '@/components/Button'
 import { withApollo } from '@/utils/withApollo'
+import Loader from '@/components/Loader'
 
 const Home: NextPage<{}> = () => {
-  const { push } = useRouter()
+  const { push, replace } = useRouter()
+
+  useEffect(() => {
+    replace('/stories')
+  }, [replace])
+
+  return <Loader />
 
   return (
     <div className={styles.home}>
