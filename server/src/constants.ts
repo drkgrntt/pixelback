4a2c7af20bd7e9cdacaf5cc6d32df657836c60c8
drkgrntt <drkgrntt@gmail.com>
2021-01-26 18:14:53 -0600

    use lerna

--- server/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const __prod__ = process.env.NODE_ENV === 'production'
-export const templates = {
-  feedback: 'feedback',
-  passwordReset: 'passwordReset',
-  newStory: 'newStory',
-}
--- web/next-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-
-declare var __NEXT_DATA__
-declare var __APOLLO_CLIENT__
-declare var GA_INITIALIZED
