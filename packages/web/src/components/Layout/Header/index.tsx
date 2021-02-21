7c31f3df5c260d1bffd66fe4bc5af7c71ca0a89c
drkgrntt <drkgrntt@gmail.com>
2021-02-21 15:33:18 -0600

    fixed header

--- packages/web/src/components/Layout/Header/index.tsx
+++ packages/web/src/components/Layout/Header/index.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react'
 import styles from './Header.module.scss'
 
 const Header: React.FC<{}> = () => {
+  const [isTop, setIsTop] = useState(true)
+  useEffect(() => {
+    const scrollListener = () => {
+      if (isTop && window.scrollY >= 25) {
+        setIsTop(false)
+      } else if (!isTop && window.scrollY < 25) {
+        setIsTop(true)
+      }
+    }
+    document.addEventListener('scroll', scrollListener)
+    return () =>
+      document.removeEventListener('scroll', scrollListener)
+  }, [isTop])
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${!isTop && styles.mini}`}>
       <div className={styles.content}>
         <h1 className={styles.title}>
           Pixelback
