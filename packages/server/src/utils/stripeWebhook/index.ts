f1b8cec75775ceedd64023b5ec8b8e218be6244b
drkgrntt <drkgrntt@gmail.com>
2021-02-24 21:32:54 -0600

    Handle failed subscription payments

--- packages/server/src/utils/stripeWebhook/index.ts
+++ packages/server/src/utils/stripeWebhook/index.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
 import Stripe from 'stripe'
+import handleFailedInvoicePayment from './handleFailedInvoicePayment'
 
-export default (req: Request, res: Response) => {
+export default async (req: Request, res: Response) => {
   const config: Stripe.StripeConfig = { apiVersion: '2020-08-27' }
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, config)
   const sig = req.headers['stripe-signature'] as string
 
-  let event
+  let event: Stripe.Event
 
   try {
     if (process.env.STRIPE_WEBHOOK_SECRET !== 'false') {
@@ -19,15 +20,19 @@ export default (req: Request, res: Response) => {
       event = JSON.parse(req.body)
     }
   } catch (err) {
-    res.status(400).send(`Webhook Error: ${err.message}`)
+    return res.status(400).send(`Webhook Error: ${err.message}`)
   }
 
   // Handle the event
   switch (event.type) {
+    case 'invoice.payment_failed':
+      await handleFailedInvoicePayment(event)
+      break
+
     default:
       console.log(`Unhandled event type ${event.type}`)
   }
 
   // Return a res to acknowledge receipt of the event
-  res.send({ received: true })
+  return res.send({ received: true })
 }
